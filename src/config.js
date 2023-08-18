d57a22fd284331f35d1a30f89cc3abdf0a7b7aea
Preetham-Jayam <preethamjayam2004@gmail.com>
2023-08-19 00:10:42 +0530

    using env variables

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAdwiIJ0fv297Zfkuu2PPFaVpGWF7vD7x0",
-  authDomain: "blogsite-e9d33.firebaseapp.com",
-  projectId: "blogsite-e9d33",
-  storageBucket: "blogsite-e9d33.appspot.com",
-  messagingSenderId: "869265888001",
-  appId: "1:869265888001:web:8c6cff45c64a789b534100"
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`
 };
 
 
